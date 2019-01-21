cf682c0eb7d041826928fc6c7dd3c4d51f3278f9
Adam Uhlir <uhlir.a@gmail.com>
2019-01-21 10:28:16 -0800

    Adding pytest markers for unit/integration tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,14 @@
-from pathlib import Path
-
 import pytest
-from pytest_mock import MockFixture
 
-# @pytest.fixture(scope="session", autouse=True)
-# def set_default_config(pytestconfig):
-#     mocker = MockFixture(pytestconfig)
-#
-#     mocker.patch.object(config.IniConfigMixin, 'DEFAULT_CONFIG_PATH')
-#     config.IniConfigMixin.DEFAULT_CONFIG_PATH.return_value = str(Path(__file__) / 'configs' / 'default.config')
-#
-#     yield
-#     mocker.stopall()
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if item.fspath is None:
+            continue
+
+        if 'integration' in str(item.fspath):
+            item.add_marker(pytest.mark.integration)
+
+        if 'unit' in str(item.fspath):
+            item.add_marker(pytest.mark.unit)
+
